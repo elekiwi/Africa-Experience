9be81a8b4ccf391dc485c73549d6084a01dee1cf
LeoMe <eleomerelo@gmail.com>
2024-02-08 12:09:47 +0100

    - Add project commit

---
+++ Africa Experience/Africa Experience/Africa_ExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_ExperienceApp.swift
+//  Africa Experience
+//
+//  Created by Leo Merelo on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Africa_ExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa Experience/Africa Experience/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa Experience
+//
+//  Created by Leo Merelo on 8/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
